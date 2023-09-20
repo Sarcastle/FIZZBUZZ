f4d3c67039d1248a3b3ed569f3ee48e101fd484b
Felicity Graham <Felicity.rose.graham@outlook.com>
2023-09-19 21:06:37 -0700

    validating input

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
-y = int(input('Input max number for FIZZBUZZ\n'))
+response = input('Input max number for FIZZBUZZ\n')
 
-for x in range(1,y):
-    isfizz = x % 3 == 0
-    isbuzz = x % 5 == 0
-    isfizzbuzz = isfizz and isbuzz
+if not response.isnumeric():
+    print('Please enter a valid number.')
+else:
+    y = int(response)
+    y +=1 
+    for x in range(1,y):
+        isfizz = x % 3 == 0
+        isbuzz = x % 5 == 0
+        isfizzbuzz = isfizz and isbuzz
 
-    if isfizzbuzz:
-        print('fizzbuzz')
-    elif isbuzz: 
-        print('buzz')
-    elif isfizz:
-        print('fizz')
-    else: 
-        print(f'{x}')
+        if isfizzbuzz:
+            print('fizzbuzz')
+        elif isbuzz: 
+            print('buzz')
+        elif isfizz:
+            print('fizz')
+        else: 
+            print(f'{x}')
